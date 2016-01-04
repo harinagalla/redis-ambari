79cd252ef4ff2cff8f9230e4495d509639f19f36
harinagalla <harinagalla@users.noreply.github.com>
2016-01-04 16:49:25 -0500

    Update master.py

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -31,12 +31,12 @@ class Master(Script):
   	  Execute('wget ' + stable_package + ' -O ' + params.temp_file + ' -a ' + params.redis_log_file, user=params.redis_user)
   	  Execute('tar zxf ' + params.temp_file+' -C ' + params.redis_install_dir + ' >> ' + params.redis_log_file, user=params.redis_user)
   	  Execute('cd '+params.redis_install_dir+'/redis-3.0.6')
-  	  Execute('make')
-  	  Execute('make test' + params.redis_log_file, user=params.redis_user)
-  	  Execute('make install' + params.redis_log_file, user=params.redis_user)
+  	  Execute('sudo make')
+  	  Execute('sudo make test')
+  	  Execute('sudo make install')
   	  Execute('cd utils')
-  	  Execute('chmod +x install_server.sh' + params.redis_log_file, user=params.redis_user)
-  	  Execute('./install_server.sh' + params.redis_log_file, user=params.redis_user)
+  	  Execute('sudo chmod +x install_server.sh')
+  	  Execute('sudo ./install_server.sh')
   	self.configure(env,True)
 	
   def create_linux_user(self, user, group):
