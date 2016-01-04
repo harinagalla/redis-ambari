612faf61eefdb616384b1882db6755a4fdba7326
harinagalla <harinagalla@users.noreply.github.com>
2016-01-04 13:11:48 -0500

    Update master.py

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -21,15 +21,15 @@ class Master(Script):
   	  group=params.redis_group,
   	  recursive=True
   	  )
-  	Execute('touch ' + params.redis_log, user=params.redis_user)
+  	Execute('touch ' + params.redis_log_file, user=params.redis_user)
   	Execute('rm -rf ' + params.redis_dir, ignore_failures=True)
   	Execute('mkdir -p ' + params.redis_dir)
   	Execute('chown -R ' + params.redis_user + ':' + params.redis_group + ' ' + params.redis_dir)
   	Execute('echo Installing pachages')
   	
   	if not os.path.exists(params.temp_file):
-  	  Execute('wget ' + stable_package + ' -O ' + params.temp_file + ' -a ' + params.redis_log, user=params.redis_user)
-  	  Execute('tar xvzf ' + params.temp_file+' -C ' + params.redis_install_dir + ' >> ' + params.redis_log, user=params.redis_user)
+  	  Execute('wget ' + stable_package + ' -O ' + params.temp_file + ' -a ' + params.redis_log_file, user=params.redis_user)
+  	  Execute('tar xvzf ' + params.temp_file+' -C ' + params.redis_install_dir + ' >> ' + params.redis_log_file, user=params.redis_user)
   	  Execute('cd '+params.redis_install_dir)
   	  Execute('make')
   	  Execute('make test')
@@ -54,7 +54,7 @@ class Master(Script):
   def stop(self, env):
 	  import params
 	  import status_params
-	  Execute('service redis stop >>' + params.redis_log, user= params.redis_user)
+	  Execute('service redis stop >>' + params.redis_log_file, user= params.redis_user)
 	  Execute('rm ' + status_params.redis_pid_file)
 	  
   def start(self, env):
@@ -62,13 +62,13 @@ class Master(Script):
 	  import status_params
 	  self.configure(env)
 	  Execute('echo pid file ' + status_params.redis_pid_file)
-	  Execute('service redis-server start >>' + params.redis_log, user=params.redis_user)
+	  Execute('service redis-server start >>' + params.redis_log_file, user=params.redis_user)
 	
   def status(self, env):
 	  import params
 	  import status_params
 	  check_process_status(status_params.redis_pid_file)
-	  Execute('service redis status >>' + params.redis_log, user=params.redis_user)
+	  Execute('service redis status >>' + params.redis_log_file, user=params.redis_user)
 	
   def set_conf_bin(self, env):
 	  import params
