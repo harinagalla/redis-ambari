7d7f2b9749d3b27ca788c2f9cc99bff63a295964
harinagalla <harinagalla@users.noreply.github.com>
2016-01-04 09:04:20 -0500

    Update master.py

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -23,7 +23,7 @@ class Master(Script):
   	  )
   	Execute('touch ' + params.redis_log_file, user=params.redis_user)
   	Execute('rm -rf ' + params.redis_dir, ignore_failures=True)
-  	Ececute('mkdir -p ' + params.redis_dir)
+  	Execute('mkdir -p ' + params.redis_dir)
   	Execute('chown -R ' + params.redis_user + ':' + params.redis_group + ' ' + params.redis_dir)
   	Execute('echo Installing pachages')
   	
